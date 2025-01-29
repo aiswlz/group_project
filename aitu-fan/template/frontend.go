59d48122eeb1d86fa0bce7d23fb31cfe06b9b8e6
aiswlz <153474803+aiswlz@users.noreply.github.com>
2025-01-29 11:14:20 +0500

    added register and login handler

--- aitu-fan/template/frontend.go
+++ aitu-fan/template/frontend.go
@@ -10,6 +10,8 @@ var templates = template.Must(template.ParseGlob("template/*.html"))
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/login", loginHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	log.Println("Server running on :8080")
@@ -18,4 +20,19 @@ func main() {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "home.html", nil)
+}
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	templates.ExecuteTemplate(w, "register.html", nil)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	templates.ExecuteTemplate(w, "login.html", nil)
 }
