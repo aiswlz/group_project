73f719b8cb2d07cb31e3a2a72a110888d27482ce
DaniyaSeitzhaparova <153476221+DaniyaSeitzhaparova@users.noreply.github.com>
2025-01-29 16:08:18 +0500

    Changed declarations for db.go

--- aitu-fan/database/db.go
+++ aitu-fan/database/db.go
@@ -33,7 +33,6 @@ func ConnectDatabase() {
 	DB = gormDB
 	fmt.Println("PostgreSQL connected successfully with GORM!")
 
-	// Initialize sql.DB connection
 	sqlDB, err = sql.Open("postgres", dsn)
 	if err != nil {
 		log.Fatal("Error connecting to the database using sql.DB:", err)
