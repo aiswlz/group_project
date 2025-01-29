16c20bc02aa862609a9cdf9b7725f448d0666a89
aigera <153474527+aigerim1q@users.noreply.github.com>
2025-01-29 14:53:14 +0500

    Create auth.go

---
+++ aitu-fan/middleware/auth.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var jwtSecret = []byte("supersecretkey")
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Token is missing", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "user", token.Claims)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
