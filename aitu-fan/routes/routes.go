296d1823bb328bdb8f472df0ebf6216001ec6a80
DaniyaSeitzhaparova <153476221+DaniyaSeitzhaparova@users.noreply.github.com>
2025-01-29 15:13:05 +0500

    Creating middleware for admin-only access

---
+++ aitu-fan/routes/routes.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/group-project/aitu-fan/aitu-fan/handlers"
+	"github.com/group-project/aitu-fan/aitu-fan/middleware"
+	"net/http"
+)
+
+func SetupRoutes() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/register", handlers.RegisterUser).Methods("POST")
+	r.HandleFunc("/login", handlers.LoginUser).Methods("POST")
+
+	api := r.PathPrefix("/api").Subrouter()
+	api.Use(middleware.AuthMiddleware)
+	api.HandleFunc("/protected", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Access to the protected route"))
+	}).Methods("GET")
+
+	admin := r.PathPrefix("/admin").Subrouter()
+	admin.Use(middleware.AdminMiddleware)
+	admin.HandleFunc("/dashboard", handlers.AdminDashboardHandler).Methods("GET")
+
+	return r
+}
