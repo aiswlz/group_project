8b9c8bbe5187c9b8027cd6b94d330c678073a4a4
aiswlz <153474803+aiswlz@users.noreply.github.com>
2025-01-29 15:56:11 +0500

    Update main.go

--- aitu-fan/main.go
+++ aitu-fan/main.go
@@ -2,14 +2,90 @@ package main
 
 import (
 	"fmt"
-	"github.com/group-project/aitu-fan/aitu-fan/routes"
+	"html/template"
 	"log"
 	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+	store     = sessions.NewCookieStore([]byte("secret-key"))
+	mu        sync.Mutex
+	users     = make(map[string]string)
 )
 
 func main() {
-	r := routes.SetupRoutes()
+	r := mux.NewRouter()
 
-	fmt.Println(" Server running on port 8080")
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/register", registerHandler).Methods("GET", "POST")
+	r.HandleFunc("/login", loginHandler).Methods("GET", "POST")
+	r.HandleFunc("/logout", logoutHandler).Methods("GET")
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	fmt.Println("Server running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	username, _ := session.Values["username"].(string)
+	templates.ExecuteTemplate(w, "home.html", map[string]string{"Username": username})
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(w, "Error hashing password", http.StatusInternalServerError)
+			return
+		}
+
+		mu.Lock()
+		users[username] = string(hashedPassword)
+		mu.Unlock()
+
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	templates.ExecuteTemplate(w, "register.html", nil)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		mu.Lock()
+		hashedPassword, exists := users[username]
+		mu.Unlock()
+
+		if !exists || bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)) != nil {
+			http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+			return
+		}
+
+		session, _ := store.Get(r, "session")
+		session.Values["username"] = username
+		session.Save(r, w)
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	templates.ExecuteTemplate(w, "login.html", nil)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	delete(session.Values, "username")
+	session.Save(r, w)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
