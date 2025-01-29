a163ed444aa4e7c2434ee7ff2291e2e7e6b84f59
DaniyaSeitzhaparova <153476221+DaniyaSeitzhaparova@users.noreply.github.com>
2025-01-29 15:31:09 +0500

    Refining the main function

--- aitu-fan/main.go
+++ aitu-fan/main.go
@@ -1,5 +1,15 @@
-<<<<<<< Updated upstream
-package aitu_fango
-=======
-package aitu_fan
->>>>>>> Stashed changes
+package main
+
+import (
+	"fmt"
+	"github.com/group-project/aitu-fan/aitu-fan/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := routes.SetupRoutes()
+
+	fmt.Println(" Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
