f0460f76ec85334ffa4d1bf4d64d29fc2a62f24d
aigera <153474527+aigerim1q@users.noreply.github.com>
2025-02-16 20:05:43 +0500

    Create gallery.go

---
+++ aitu-fan/handlers/gallery.go
@@ -0,0 +1,100 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"html/template"
+	"net/http"
+	"time"
+
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+	"github.com/group-project/aitu-fan/config"
+	"github.com/group-project/aitu-fan/database"
+	"github.com/group-project/aitu-fan/middleware"
+	"github.com/group-project/aitu-fan/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type GalleryPageData struct {
+	GalleryItems []models.GalleryItem
+}
+
+func GetGallery(w http.ResponseWriter, r *http.Request) {
+	collection := database.GetCollection("gallery")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		http.Error(w, "Error fetching gallery", http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(ctx)
+
+	var gallery []models.GalleryItem
+	if err = cursor.All(ctx, &gallery); err != nil {
+		http.Error(w, "Error processing data", http.StatusInternalServerError)
+		return
+	}
+
+	pageData := GalleryPageData{
+		GalleryItems: gallery,
+	}
+
+	tmpl, err := template.ParseFiles("templates/gallery.html")
+	if err != nil {
+		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	tmpl.Execute(w, pageData)
+}
+
+func UploadGalleryItem(w http.ResponseWriter, r *http.Request) {
+	collection := database.GetCollection("gallery")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	userID, err := middleware.GetUserID(r)
+	if err != nil {
+		http.Error(w, "User not authorized", http.StatusUnauthorized)
+		return
+	}
+
+	r.ParseMultipartForm(10 << 20)
+	title := r.FormValue("title")
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "File not uploaded", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	uploadResult, err := config.Cloudinary.Upload.Upload(ctx, file, uploader.UploadParams{
+		Folder: "aitu-fan-gallery",
+	})
+	if err != nil {
+		http.Error(w, "Error uploading image to Cloudinary", http.StatusInternalServerError)
+		return
+	}
+
+	galleryItem := models.GalleryItem{
+		ID:         primitive.NewObjectID(),
+		Title:      title,
+		URL:        uploadResult.SecureURL,
+		UploadedBy: userID,
+		UploadedAt: time.Now(),
+	}
+
+	_, err = collection.InsertOne(ctx, galleryItem)
+	if err != nil {
+		http.Error(w, "Error saving image to database", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(galleryItem)
+}
