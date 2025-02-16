5d32722ffb89baee56a8a0eedfcadd43b37ecc9f
aigera <153474527+aigerim1q@users.noreply.github.com>
2025-02-16 20:07:11 +0500

    Update users.go

--- aitu-fan/models/users.go
+++ aitu-fan/models/users.go
@@ -2,13 +2,14 @@ package models
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	ID           uint      `gorm:"primaryKey" json:"id"`
-	Username     string    `gorm:"type:varchar(100);unique;not null" json:"username"`
-	Email        string    `gorm:"type:varchar(100);unique;not null" json:"email"`
-	PasswordHash string    `gorm:"type:text;not null" json:"password_hash"`
-	Role         string    `gorm:"type:varchar(50);default:'user'" json:"role"`
-	CreatedAt    time.Time `gorm:"autoCreateTime" json:"created_at"`
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Username     string             `json:"username"`
+	PasswordHash string             `json:"password_hash"`
+	Role         string             `json:"role"`
+	CreatedAt    time.Time          `json:"created_at"`
 }
