cda61f0d93090ae5d267b5c48b90d6565cf538e1
aigera <153474527+aigerim1q@users.noreply.github.com>
2025-02-16 20:04:27 +0500

    Update auth.go

--- aitu-fan/handlers/auth.go
+++ aitu-fan/handlers/auth.go
@@ -1,88 +1,128 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"time"
+	"unicode"
 
-	"github.com/golang-jwt/jwt/v4"
-	"github.com/group-project/aitu-fan/aitu-fan/models"
+	"github.com/group-project/aitu-fan/database"
+	"github.com/group-project/aitu-fan/models"
+	"github.com/group-project/aitu-fan/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 )
 
-var jwtSecret = []byte("supersecretkey")
-
-var users = []models.User{}
-
-type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+func sendErrorResponse(w http.ResponseWriter, message string, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(map[string]string{"error": message})
 }
 
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
-	var user models.User
+	var user struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		http.Error(w, "Error processing request data", http.StatusBadRequest)
+		sendErrorResponse(w, "Error", http.StatusBadRequest)
 		return
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.PasswordHash), bcrypt.DefaultCost)
+	if len(user.Username) < 3 || len(user.Username) > 15 {
+		sendErrorResponse(w, "Username has to consist of 3-15 symbols", http.StatusBadRequest)
+		return
+	}
+
+	if len(user.Password) < 6 || len(user.Password) > 8 || !hasSpecialChar(user.Password) {
+		sendErrorResponse(w, "Password has to contain 6-8 symbols and at least 1 special symbol", http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
-		http.Error(w, "Error hashing password", http.StatusInternalServerError)
+		sendErrorResponse(w, "Password hashing error", http.StatusInternalServerError)
 		return
 	}
-	user.PasswordHash = string(hashedPassword)
 
-	user.ID = len(users) + 1
-	users = append(users, user)
+	newUser := models.User{
+		ID:           primitive.NewObjectID(),
+		Username:     user.Username,
+		PasswordHash: string(hashedPassword),
+		CreatedAt:    time.Now(),
+		Role:         "user",
+	}
 
+	collection := database.Client.Database("aitu-fan").Collection("users")
+	_, err = collection.InsertOne(context.TODO(), newUser)
+	if err != nil {
+		sendErrorResponse(w, "Failed to save user", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Registration successful"})
+	json.NewEncoder(w).Encode(map[string]string{"message": "Successful!"})
+}
+
+func hasSpecialChar(s string) bool {
+	for _, char := range s {
+		if unicode.IsSymbol(char) || unicode.IsPunct(char) {
+			return true
+		}
+	}
+	return false
 }
 
 func LoginUser(w http.ResponseWriter, r *http.Request) {
-	var req LoginRequest
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		http.Error(w, "Error processing request data", http.StatusBadRequest)
+		http.Error(w, "Error", http.StatusBadRequest)
 		return
 	}
 
-	var user *models.User
-	for i := range users {
-		if users[i].Email == req.Email {
-			user = &users[i]
-			break
-		}
-	}
+	collection := database.Client.Database("aitu-fan").Collection("users")
 
-	if user == nil {
+	var user models.User
+	err = collection.FindOne(context.TODO(), bson.M{"username": req.Username}).Decode(&user)
+	if err != nil {
 		http.Error(w, "User not found", http.StatusUnauthorized)
 		return
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(req.Password))
 	if err != nil {
-		http.Error(w, "Invalid password", http.StatusUnauthorized)
+		http.Error(w, "Wrong password", http.StatusUnauthorized)
 		return
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user_id": user.ID,
-		"role":    user.Role,
-		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	tokenString, err := utils.GenerateToken(user.ID.Hex(), user.Role)
+	if err != nil {
+		http.Error(w, "Token generation error", http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "token",
+		Value:    tokenString,
+		Path:     "/",
+		HttpOnly: true,
 	})
 
-	tokenString, err := token.SignedString(jwtSecret)
-	if err != nil {
-		http.Error(w, "Error generating token", http.StatusInternalServerError)
-		return
-	}
-
-	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
-}
-
-func AdminDashboardHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to the Admin Dashboard!"))
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"message":   "Successful!",
+		"token":     tokenString,
+		"user_id":   user.ID.Hex(),
+		"user_role": user.Role,
+	})
 }
